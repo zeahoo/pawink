480fd0c76487044192baaa1f9d56293c7e6e3db9
zeahoo <zeahoooo@gmail.com>
2024-01-10 10:31:58 +0800

    add prettier support

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
     <main>
       <Button>Pawink</Button>
     </main>
-  )
+  );
 }
