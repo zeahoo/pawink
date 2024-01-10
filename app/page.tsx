39a30191c318daefc62114dd0a8d5c85ac3f960d
zeahoo <zeahoooo@gmail.com>
2024-01-10 09:57:47 +0800

    add shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main>Hello pawink</main>
+    <main>
+      <Button>Pawink</Button>
+    </main>
   )
 }
